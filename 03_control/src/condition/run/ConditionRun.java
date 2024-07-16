b487b79c4cf7f34a788769fb6e88763ac5399a3b
Chowonchan <cwc2264@gmail.com>
2024-07-16 17:43:40 +0900

    2024 java if  연산

---
+++ 03_control/src/condition/run/ConditionRun.java
@@ -0,0 +1,24 @@
+package condition.run;
+
+/**
+ * 조건문 기능 실행용 클래스
+ */
+
+import condition.ex.ConditionEx;
+
+public class ConditionRun {
+	public static void main(String[] args) {
+		
+		// ConditionEx 객체 생성
+		ConditionEx ex = new ConditionEx();
+		
+//		ex.method1();
+//		ex.method2();
+//		ex.method3();
+		
+//		ex.displayMenu();
+		
+		ex.practice();
+	}
+
+}
